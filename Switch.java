c01886a77f6713985f6149a0a92be08cfd1629fc
Hemanth-eng877 <hemanthguru72@gmail.com>
2026-04-06 13:01:15 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class GradeSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter grade (A/B/C/D/F): ");
+        char grade = sc.next().charAt(0);
+
+        switch(grade) {
+            case 'A':
+                System.out.println("Excellent");
+                break;
+            case 'B':
+                System.out.println("Very Good");
+                break;
+            case 'C':
+                System.out.println("Good");
+                break;
+            case 'D':
+                System.out.println("Pass");
+                break;
+            case 'F':
+                System.out.println("Fail");
+                break;
+            default:
+                System.out.println("Invalid Grade");
+        }
+    }
+}
